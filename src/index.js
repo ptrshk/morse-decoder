1afdb7bacb9dfd03839638017b85eee8ed28d757
ptrshk <119881112+ptrshk@users.noreply.github.com>
2023-02-13 20:11:36 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    newString = String(expr);
+    let symbol = '';
+    let final = '';
+    let sign = '';
+    let morse = '';
+    
+    for(let a = 0; a < newString.length + 1; a++)
+    {
+        if ((a == 0) || !(a % 10 == 0)){
+            symbol = symbol + newString[a];
+        }
+        else
+        {
+                for (let b = 0; b < symbol.length + 1; b++)
+                {
+                if ((b == 0) || !(b % 2 == 0)){
+                    sign = sign + symbol[b];
+                }
+                else
+                {
+                    if (sign == '10')
+                    {
+                        morse = morse + '.';
+                    }
+                    else if (sign == '11')
+                    {
+                        morse = morse + '-';
+                    }
+                    sign = symbol[b];
+                }
+            }
+            for(let el in MORSE_TABLE)
+            {
+                if(el == morse)
+                {
+                    final = final + MORSE_TABLE[el];
+                }
+            }
+            if (symbol[2] == '*')
+            {
+                final = final + ' ';
+            }
+            symbol = newString[a];
+            morse = '';   
+            sign = ''; 
+            }
+    }
+    return final;
 }
 
 module.exports = {
